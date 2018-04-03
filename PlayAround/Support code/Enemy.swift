a2b0f17cb46fe3c077e8b0601642d825188e2da2
Bret Williams <bret@brets-mbp.tx.rr.com>
2018-04-02 20:29:53 -0500

    Got a bug but i'm tired.

--- PlayAround/Support code/Enemy.swift
+++ PlayAround/Support code/Enemy.swift
@@ -26,12 +26,48 @@ class Enemy : SKSpriteNode {
     var allowMovement:Bool = false
     var movementActions = [String]()
     
+    var waitTimeForRandomWalking:TimeInterval = -1
+    var randomWaitTimeRange:UInt32 = 3
+    var walkTime:TimeInterval = 1
+    var walkDistance:CGFloat = 50
+    
     //ranged attack
     
     
     //melee attack
     
+    //animation
+    var frontWalk:String = ""
+    var frontIdle:String = ""
+    var frontMelee:String = ""
+    var frontRanged:String = ""
+    var frontHurt:String = ""
+    
+    var backWalk:String = ""
+    var backIdle:String = ""
+    var backMelee:String = ""
+    var backRanged:String = ""
+    var backHurt:String = ""
+    
+    var leftWalk:String = ""
+    var leftIdle:String = ""
+    var leftMelee:String = ""
+    var leftRanged:String = ""
+    var leftHurt:String = ""
+    
+    var rightWalk:String = ""
+    var rightIdle:String = ""
+    var rightMelee:String = ""
+    var rightRanged:String = ""
+    var rightHurt:String = ""
+    
+    var rightDying:String = ""
+    var leftDying:String = ""
+    var backDying:String = ""
+    var frontDying:String = ""
 
+    var hasIdleAnimation:Bool = false
+    
     func setUpEnemy(theDict:[String:Any]) {
         
         print(self.name!)
@@ -52,6 +88,12 @@ class Enemy : SKSpriteNode {
                     
                 }
                 
+            case "Animation":
+                
+                if (value is [String:Any]) {
+                    
+                    sortAnimationDict(theDict: value as! [String:Any])
+                }
                 
             default:
                 continue
@@ -101,10 +143,91 @@ class Enemy : SKSpriteNode {
                 
                 
                 
+            } else if (movementType == .random) {
+                
+                if (allowMovement) {
+                    
+                    if (self.action(forKey: "Movement") == nil
+                        && self.action(forKey: "Hurt") == nil
+                        && self.action(forKey: "Attack") == nil) {
+           
+                        walkRandom()
+                        
+                    }
+                }
+                
+            }
+
+        }
+        
+    }
+    
+    func walkRandom() {
+        
+        self.removeAction(forKey: "Idle")
+        var wait:SKAction = SKAction()
+        
+        if (waitTimeForRandomWalking > -1)  {
+            
+            wait = SKAction.wait(forDuration: waitTimeForRandomWalking)
+            
+        } else {
+            
+            let waitTime = arc4random_uniform(randomWaitTimeRange)
+            wait = SKAction.wait(forDuration: TimeInterval(waitTime))
+            
+        }
+        
+        let endMove:SKAction = SKAction.run {
+            
+            self.removeAction(forKey: "WalkAnimation")
+            
+        }
+        
+        print("walkDistance = " + String(format: "%.2f",walkDistance))
+        print("walkTime = " + String(walkTime))
+        
+        let randomNum = arc4random_uniform(4)
+        //print (randomNum)
+        switch randomNum {
+        case 0:
+            print ("frontWalk = " + frontWalk)
+            if let walk = SKAction(named: frontWalk) {
+                let repeatWalk:SKAction = SKAction.repeatForever(walk)
+                self.run(repeatWalk, withKey: "WalkAnimation")
             }
             
+            let move:SKAction = SKAction.moveBy(x: 0, y: -walkDistance, duration: walkTime)
+            self.run(SKAction.sequence([ move, endMove, wait ]), withKey: "WalkAnimation")
+        case 1:
+            print ("backWalk = " + backWalk)
+            if let walk = SKAction(named: backWalk) {
+                let repeatWalk:SKAction = SKAction.repeatForever(walk)
+                self.run(repeatWalk, withKey: "WalkAnimation")
+            }
             
+            let move:SKAction = SKAction.moveBy(x: 0, y: walkDistance, duration: walkTime)
+            self.run(SKAction.sequence([ move, endMove, wait ]), withKey: "WalkAnimation")
+        case 2:
+            print("leftWalk = " + leftWalk)
+            if let walk = SKAction(named: leftWalk) {
+                let repeatWalk:SKAction = SKAction.repeatForever(walk)
+                self.run(repeatWalk, withKey: "WalkAnimation")
+            }
             
+            let move:SKAction = SKAction.moveBy(x: -walkDistance, y:0, duration: walkTime)
+            self.run(SKAction.sequence([ move, endMove, wait ]), withKey: "WalkAnimation")
+        case 3:
+            print("rightWalk = " + rightWalk)
+            if let walk = SKAction(named: rightWalk) {
+                let repeatWalk:SKAction = SKAction.repeatForever(walk)
+                self.run(repeatWalk, withKey: "WalkAnimation")
+            }
+            
+            let move:SKAction = SKAction.moveBy(x: walkDistance, y: 0, duration: walkTime)
+            self.run(SKAction.sequence([ move, endMove, wait ]), withKey: "WalkAnimation")
+        default:
+            break
         }
         
     }
@@ -144,6 +267,109 @@ class Enemy : SKSpriteNode {
     
     //MARK:  Sorting stuff
     
+    func sortAnimationDict(theDict: [String:Any]) {
+        
+        for (key,value) in theDict {
+            print ("key = " + key)
+            switch key {
+            case "Back":
+                if let backDict:[String:Any] = value as? [String:Any] {
+                    for (backKey, backValue) in backDict {
+                        print ("backKey = " + backKey)
+                        print ("backValue = " + (backValue as! String))
+                        switch backKey {
+                        case "Walk":
+                            backWalk = backValue as! String
+                        case "Idle":
+                            backIdle = backValue as! String
+                        case "Melee":
+                            backMelee = backValue as! String
+                        case "Ranged":
+                            backRanged = backValue as! String
+                        case "Hurt":
+                            backHurt = backValue as! String
+                        case "Dying":
+                            backDying = backValue as! String
+                        default:
+                            continue
+                        }
+                    }
+                }
+            case "Front":
+                if let frontDict:[String:Any] = value as? [String:Any] {
+                    for (frontKey, frontValue) in frontDict {
+                        switch frontKey {
+                        case "Walk":
+                            frontWalk = frontValue as! String
+                        case "Idle":
+                            frontIdle = frontValue as! String
+                            hasIdleAnimation = true
+                        case "Melee":
+                            frontMelee = frontValue as! String
+                        case "Ranged":
+                            frontRanged = frontValue as! String
+                        case "Hurt":
+                            frontHurt = frontValue as! String
+                        case "Dying":
+                            frontDying = frontValue as! String
+                        default:
+                            continue
+                        }
+                    }
+                }
+            case "Left":
+                if let leftDict:[String:Any] = value as? [String:Any] {
+                    for (leftKey, leftValue) in leftDict {
+                        switch leftKey {
+                        case "Walk":
+                            leftWalk = leftValue as! String
+                        case "Idle":
+                            leftIdle = leftValue as! String
+                        case "Melee":
+                            leftMelee = leftValue as! String
+                        case "Ranged":
+                            leftRanged = leftValue as! String
+                        case "Hurt":
+                            leftHurt = leftValue as! String
+                        case "Dying":
+                            leftDying = leftValue as! String
+                        default:
+                            continue
+                        }
+                    }
+                }
+            case "Right":
+                if let rightDict:[String:Any] = value as? [String:Any] {
+                    for (rightKey, rightValue) in rightDict {
+                        switch  rightKey {
+                        case "Walk":
+                            rightWalk = rightValue as! String
+                        case "Idle":
+                            rightIdle = rightValue as! String
+                        case "Melee":
+                            rightMelee = rightValue as! String
+                        case "Ranged":
+                            rightRanged = rightValue as! String
+                        case "Hurt":
+                            rightHurt = rightValue as! String
+                        case "Dying":
+                            rightDying = rightValue as! String
+                        default:
+                            continue
+                        }
+                    }
+                }
+            default:
+                continue
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    
     func sortMovementDict(theDict: [String:Any]) {
         
         for (key,value) in theDict {
@@ -163,6 +389,45 @@ class Enemy : SKSpriteNode {
                     
                     moveIfPlayerWithin = value as! CGFloat
                     
+                }
+                
+            case "WalkRandomly":
+                
+                if (value is Bool) {
+                    
+                    if (value as! Bool == true) {
+                        
+                        movementType = .random
+                        
+                    }
+                }
+            case "WaitTime":
+                
+                if (value is TimeInterval) {
+                    
+                    waitTimeForRandomWalking = value as! TimeInterval
+                }
+                
+            case "WalkTime":
+                
+                if (value is TimeInterval) {
+                    
+                    walkTime = value as! TimeInterval
+                    
+                }
+                
+            case "WalkDistance":
+                
+                if (value is CGFloat) {
+                    
+                    walkDistance = value as! CGFloat
+                }
+                
+            case "RandomWaitTime":
+                
+                if (value is UInt32) {
+                    
+                    randomWaitTimeRange = value as! UInt32
                 }
                 
             default:
